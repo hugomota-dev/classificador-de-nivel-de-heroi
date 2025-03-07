0549d81b9529349bbdf24f307860795ec9598e6e
Hugo Mota <hugomota.lab@gmail.com>
2025-03-07 19:13:30 +0000

    Primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# classificador-de-nivel-de-heroi
+#classificador-de-nivel-de-heroi
+
+Desafio DIO 1 - GFT Start #6 - Lógica de Programação
+
+Projeto básico feito em Javascript.
---
+++ index.js
@@ -0,0 +1,49 @@
+let nomeDoHeroi = "Hugo Mota";
+let xpDoHeroi = 20001;
+let nivelDoHeroi = " ";
+const textoXp = "Experiência (xp) atual do herói: " + xpDoHeroi
+if(xpDoHeroi <= 1000){
+    nivelDoHeroi = "Ferro";
+    console.log(`O herói de nome ${nomeDoHeroi} está no nível ${nivelDoHeroi}`);
+    console.log("Xp atual: " + xpDoHeroi)
+
+} else if (xpDoHeroi >1000 && xpDoHeroi <=2000){
+    nivelDoHeroi = "Bronze";
+    console.log(`O herói de nome ${nomeDoHeroi} está no nível ${nivelDoHeroi}`);
+    console.log(textoXp);
+
+} else if (xpDoHeroi >2000 && xpDoHeroi <=5000){
+    nivelDoHeroi = "Prata";
+    console.log(`O herói de nome ${nomeDoHeroi} está no nível ${nivelDoHeroi}`);
+    console.log(textoXp);
+
+} else if (xpDoHeroi >5000 && xpDoHeroi <=7000){
+    nivelDoHeroi = "Ouro";
+    console.log(`O herói de nome ${nomeDoHeroi} está no nível ${nivelDoHeroi}`);
+    console.log(textoXp);
+
+} else if (xpDoHeroi >7000 && xpDoHeroi <=8000){
+        nivelDoHeroi = "Platina";
+        console.log(`O herói de nome ${nomeDoHeroi} está no nível ${nivelDoHeroi}`);
+        console.log(textoXp);
+
+} else if (xpDoHeroi >8000 && xpDoHeroi <=9000){
+        nivelDoHeroi = "Ascendente";
+        console.log(`O herói de nome ${nomeDoHeroi} está no nível ${nivelDoHeroi}`);
+        console.log(textoXp);
+
+} else if (xpDoHeroi >9000 && xpDoHeroi <=10000){
+        nivelDoHeroi = "Imortal";
+        console.log(`O herói de nome ${nomeDoHeroi} está no nível ${nivelDoHeroi}`);
+        console.log(textoXp);
+
+} else if (xpDoHeroi >=10001){
+        nivelDoHeroi = "Radiante";
+        console.log(`O herói de nome ${nomeDoHeroi} está no nível ${nivelDoHeroi}`);
+        console.log(textoXp);
+    
+}
+
+else{
+    console.log("Nível não detectado!")
+}
